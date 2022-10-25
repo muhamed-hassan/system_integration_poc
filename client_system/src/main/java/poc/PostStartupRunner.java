932419326197f0f9f93fdb54c9fe66d3de9c379a
muhamed-hassan <mohamed.hassan.work@gmail.com>
2022-10-25 22:12:10 +0200

    code refactoring

--- client_system/src/main/java/poc/PostStartupRunner.java
+++ client_system/src/main/java/poc/PostStartupRunner.java
@@ -35,33 +35,6 @@ public class PostStartupRunner implements ApplicationRunner {
 		doRestTemplatePoc();
 	}
 
-	private void doRestTemplatePoc() {
-		var pool = new ForkJoinPool(Runtime.getRuntime().availableProcessors());
-		
-		CompletableFuture.runAsync(backendSystemClientV2::findById, pool)
-							.runAsync(backendSystemClientV2::findByPage, pool)
-							.runAsync(backendSystemClientV2::getWithClientError, pool)
-							.runAsync(backendSystemClientV2::getWithServerError, pool)
-							.runAsync(backendSystemClientV2::getWithOtherError, pool)
-							
-							.runAsync(backendSystemClientV2::deleteById, pool)
-							.runAsync(backendSystemClientV2::deleteWithClientError, pool)
-							.runAsync(backendSystemClientV2::deleteWithServerError, pool)
-							.runAsync(backendSystemClientV2::deleteWithOtherError, pool)
-							
-							.runAsync(backendSystemClientV2::updateById, pool)
-							.runAsync(backendSystemClientV2::updateByIdWithViolatingPayloadValidations, pool)
-							.runAsync(backendSystemClientV2::patchWithClientError, pool)
-							.runAsync(backendSystemClientV2::patchWithServerError, pool)
-							.runAsync(backendSystemClientV2::patchWithOtherError, pool)
-							
-							.runAsync(backendSystemClientV2::save, pool)
-							.runAsync(backendSystemClientV2::saveWithViolatingPayloadValidations, pool)
-							.runAsync(backendSystemClientV2::postWithClientError, pool)
-							.runAsync(backendSystemClientV2::postWithServerError, pool)
-							.runAsync(backendSystemClientV2::postWithOtherError, pool);
-	}
-	
 	private void doWebClientPoc() 
 			throws InterruptedException {
 		var countDownLatch = new CountDownLatch(1);
@@ -96,4 +69,36 @@ public class PostStartupRunner implements ApplicationRunner {
 		countDownLatch.await();
 	}
 
+	private void doRestTemplatePoc() {
+		var pool = new ForkJoinPool(Runtime.getRuntime().availableProcessors());
+		
+		CompletableFuture.runAsync(backendSystemClientV2::findById, pool)
+							.runAsync(backendSystemClientV2::findByPage, pool)
+							.runAsync(backendSystemClientV2::getWithClientError, pool)
+							.runAsync(backendSystemClientV2::getWithServerError, pool)
+							.runAsync(backendSystemClientV2::getWithOtherError, pool)
+							
+							.runAsync(backendSystemClientV2::deleteById, pool)
+							.runAsync(backendSystemClientV2::deleteWithClientError, pool)
+							.runAsync(backendSystemClientV2::deleteWithServerError, pool)
+							.runAsync(backendSystemClientV2::deleteWithOtherError, pool)
+							
+							.runAsync(backendSystemClientV2::updateById, pool)
+							.runAsync(backendSystemClientV2::updateByIdWithViolatingPayloadValidations, pool)
+							.runAsync(backendSystemClientV2::patchWithClientError, pool)
+							.runAsync(backendSystemClientV2::patchWithServerError, pool)
+							.runAsync(backendSystemClientV2::patchWithOtherError, pool)
+							
+							.runAsync(backendSystemClientV2::save, pool)
+							.runAsync(backendSystemClientV2::saveWithViolatingPayloadValidations, pool)
+							.runAsync(backendSystemClientV2::postWithClientError, pool)
+							.runAsync(backendSystemClientV2::postWithServerError, pool)
+							.runAsync(backendSystemClientV2::postWithOtherError, pool)
+							
+							.whenComplete((voidChain, throwable) -> {
+								displayer.print("ForkJoinPool is shutting down ...");
+								pool.shutdown();
+							});
+	}
+	
 }
