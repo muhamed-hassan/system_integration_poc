cd48726c57b4b15a58d0003029d42eb5122a51f7
Mohamed Hassan <mohamed.hassan.work@gmail.com>
2023-01-01 09:13:11 +0200

    Update EmployeeControllerV2.java

--- backend_system/src/main/java/poc/interfaces/rest/controllers/EmployeeControllerV2.java
+++ backend_system/src/main/java/poc/interfaces/rest/controllers/EmployeeControllerV2.java
@@ -14,7 +14,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,7 +29,6 @@ import poc.persistence.repositories.EmployeeRepository;
 
 @RestController
 @RequestMapping("v2/employees")
-@Validated
 public class EmployeeControllerV2 {
 	
 	@Autowired
